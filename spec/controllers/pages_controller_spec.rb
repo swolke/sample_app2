6e4d637ca626caba97e9c0acc6ecf5b0e977a041
Stefanie Wolke <st.wolky@gmx.de>
2011-11-16 20:41:20 +0100

    funktioniert wieder

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,10 +4,9 @@ describe PagesController do
  render_views
 
 #@pages = ['home', 'contact', 'about', 'help']
-  before(:each) do
-    @base_title = "!Meine Sample App! | "
-
-  end
+ before(:each) do
+   @base_title = "Ruby on Rails Sample App"
+ end
 
 # pages.each do |page|
 #     describe "GET '#{page}'" do
@@ -32,43 +31,43 @@ describe PagesController do
     it "should have the right title" do
   get 'home'
   response.should have_selector("title",
-                    :content => @base_title + "Home")
+                    :content =>  "Home")
 end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
      get 'contact'
-      response.should be_success
+      response.should be_true
     end
     it "should have the right title" do
   get 'contact'
   response.should have_selector("title",
-                    :content => @base_title + "Contact")
+                    :content =>  "Contact")
 end
   end
 
     describe "GET 'about'" do
     it "should be successful" do
       get 'about'
-      response.should be_success
+      response.should be_true
     end
       it "should have the right title" do
   get 'about'
   response.should have_selector("title",
-                                :content => @base_title + "About")
+                                :content =>  "About")
 end
     end
 
       describe "GET 'help'" do
     it "should be successful" do
       get 'help'
-      response.should be_success
+      response.should be_true
     end
            it "should have the right title" do
   get 'help'
   response.should have_selector("title",
-                    :content => @base_title + "Help")
+                    :content =>  "Help")
 end
         end
 end
